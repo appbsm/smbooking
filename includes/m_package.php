c7d84aeac39c8f348bac22de0579ade9b1537e96
BUILDERSMART\thanawat <Bsm@2000>
2024-06-18 13:56:42 +0700

    no message

---
+++ includes/m_package.php
@@ -0,0 +1,17 @@
+<?php
+
+function get_all_packages($conn) {
+	$result = array();
+ 	$sql ="SELECT * from package WHERE is_active = '1' ";
+	$stmt = sqlsrv_query($conn,$sql);
+	if( $stmt === false) {
+		die( print_r( sqlsrv_errors(), true) );
+	}
+	while($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC)){    
+		$result[] = $row;
+	} 
+	return $result;
+}
+
+
+?>
