a81e314b2f478ed97e3b3a59fa16e988c8115536
D:\BK-AA\brokerapp\broker-2 <D:\BK-AA\brokerapp\broker-2>
2024-06-24 16:33:21 +0700

    no message

--- includes/footer.php
+++ includes/footer.php
@@ -90,9 +90,9 @@
         </div>
     </footer>
 	-->
-	<div id="footer_2_section">
-        <!-- section-2 start-->
+	<div id="footer_2_section" >
         <div class="section_2 f-section_2">
+		<!--
             <div class="container">
                 <div class="section2_wrapper">
                     <div class="row" style="padding: 20px 0;">
@@ -160,14 +160,17 @@
                     </div>
                 </div>
             </div>
+			-->
+			<!--
             <div class="section2_bottom">
                 <div class="container">
-                    <div class="row">
+                    <div class="row" style="padding: 20px 0;">
                         <div class="col-lg-4 col-md-6 col-xs-12 col-sm-12">
                             <div class="section2_footer_img">
                                 <img src="images/10.png" style="width: 70px;" class="img-responsive" alt="widget3_img" />
                             </div>
                         </div>
+						
                         <div class="col-lg-8 col-md-6 col-xs-12 col-sm-12">
                             <div class="section2_footer_menu">
                                 <ul>
@@ -180,16 +183,18 @@
                                 </ul>
                             </div>
                         </div>
+						
                     </div>
                 </div>
             </div>
+			-->
         </div>
         <div class="close_wrapper">
         </div>
-        <!-- section-2 end -->
     </div>
 
 <!-- bottom footer -->
+<!--
 <div class="bottom-footer">
     <div class="container">
         <div class="row align-items-center">
@@ -218,3 +223,101 @@
         </div>
     </div>
 </div>
+-->
+<div class="col-12 card" style="marging: 0; width: 100%; box-shadow: 2px 0px 1px 2px rgba(22, 22, 26, 0.18);">
+<footer>
+    <div class="container">
+        <div class="row" style="padding: 20px 0;">
+			<div class="col-2 mb-2">
+				<div class="section2_footer_img">
+					<img src="images/10.png" style="width: 70px;" class="img-responsive" alt="widget3_img" />
+				</div>
+			</div>
+			<div class="col-md-3">
+                <h6 class="text-dark mb-2 mt-2">เมนูเว็บไซต์</h6>
+                <p class="" style="font-size: 14px;">
+					<span>
+						<i class="fa fa-long-arrow-right" aria-hidden="true"></i><a href="javascript:;"> รีสอร์ท </a>
+					</span><br/>
+					<span>
+						<i class="fa fa-long-arrow-right" aria-hidden="true"></i><a href="javascript:;"> โรงแรม </a>
+					</span><br/>
+					<span>
+						<i class="fa fa-long-arrow-right" aria-hidden="true"></i><a href="javascript:;"> คอนโด & อพาร์ทเมนต์ </a>
+					</span><br/>
+					<span>
+						<i class="fa fa-long-arrow-right" aria-hidden="true"></i><a href="javascript:;"> โปรโมชั่น & แพ็คเกจ </a>
+					</span>
+                </p>
+            </div>
+            <div class="col-md-4 mt-2">
+				<h6 class="text-dark mb-2">ติดต่อเรา</h6>
+                <p class="text-dark" style="font-size: 14px;">
+					<svg  width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+						<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+					</svg>
+					<strong class="col-xs-2" >
+						<span>&nbsp;&nbsp;บริษัท อินสตอลไดเรค จำกัด</span><br>
+						<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 905/7 ถนนพระราม 3 แขวงบางโพงพาง</span><br>
+						<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; เขตยานนาวา จ.กรุงเทพฯ 10120</span>
+					</strong>
+				</p>
+                <p class="text-dark" style="font-size: 14px;">
+					<svg  width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+						<path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+					</svg>
+					<span>&nbsp;&nbsp;เบอร์โทรศัพท์ :</span>
+					<a class="text-dark" href="tel:026834900">
+						<span>&nbsp;&nbsp;02-683-4900 ต่อ 223</span>
+					</a>
+				</p>
+                <p class="text-dark" style="font-size: 14px;">
+					<i class="fa fa-fax"style="font-size:17px;"></i>
+						<span>&nbsp;&nbsp;&nbsp;เบอร์โทรแฟกซ์ :</span>
+						<span>&nbsp;&nbsp;02-683-4949 </span>
+				</p>
+                <p class="text-dark" style="font-size: 14px;">
+					<svg  width="16" height="16" fill="currentColor" class="bi bi-line" viewBox="0 0 16 16">
+					  <path d="M8 0c4.411 0 8 2.912 8 6.492 0 1.433-.555 2.723-1.715 3.994-1.678 1.932-5.431 4.285-6.285 4.645-.83.35-.734-.197-.696-.413l.003-.018.114-.685c.027-.204.055-.521-.026-.723-.09-.223-.444-.339-.704-.395C2.846 12.39 0 9.701 0 6.492 0 2.912 3.59 0 8 0M5.022 7.686H3.497V4.918a.156.156 0 0 0-.155-.156H2.78a.156.156 0 0 0-.156.156v3.486c0 .041.017.08.044.107v.001l.002.002.002.002a.15.15 0 0 0 .108.043h2.242c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157m.791-2.924a.156.156 0 0 0-.156.156v3.486c0 .086.07.155.156.155h.562c.086 0 .155-.07.155-.155V4.918a.156.156 0 0 0-.155-.156zm3.863 0a.156.156 0 0 0-.156.156v2.07L7.923 4.832l-.013-.015v-.001l-.01-.01-.003-.003-.011-.009h-.001L7.88 4.79l-.003-.002-.005-.003-.008-.005h-.002l-.003-.002-.01-.004-.004-.002-.01-.003h-.002l-.003-.001-.009-.002h-.006l-.003-.001h-.004l-.002-.001h-.574a.156.156 0 0 0-.156.155v3.486c0 .086.07.155.156.155h.56c.087 0 .157-.07.157-.155v-2.07l1.6 2.16a.2.2 0 0 0 .039.038l.001.001.01.006.004.002.008.004.007.003.005.002.01.003h.003a.2.2 0 0 0 .04.006h.56c.087 0 .157-.07.157-.155V4.918a.156.156 0 0 0-.156-.156zm3.815.717v-.56a.156.156 0 0 0-.155-.157h-2.242a.16.16 0 0 0-.108.044h-.001l-.001.002-.002.003a.16.16 0 0 0-.044.107v3.486c0 .041.017.08.044.107l.002.003.002.002a.16.16 0 0 0 .108.043h2.242c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157H11.81v-.589h1.525c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157H11.81v-.589h1.525c.086 0 .155-.07.155-.156Z"/>
+					</svg>
+					<!--<i class="fab fa-line" style="font-size:19px;"></i>-->
+					<span>&nbsp;&nbsp;ไลน์ไอดี :</span>
+					<span>&nbsp;&nbsp;@installdirect</span>
+				</p>
+                <p class="text-dark" style="font-size: 14px;">
+					<svg  width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
+						<path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
+					</svg>
+					<span>&nbsp;&nbsp;ติ๊กต๊อก :</span>
+					<span>&nbsp;&nbsp;<span>installdirect</span>
+				</p>
+            </div>
+            <div class="col-md-3 mt-2">
+				<h6 class="text-center text-dark mb-2">แอดไลน์</h6>
+				<div class="d-flex justify-content-center" >
+					<img class="footer_image footer_image_mobile" src="images/qrcode_s.png" style="width: 50%;">
+				</div>
+            </div>
+        </div>
+		<!--
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center">© 2024 RoomScope. All Rights Reserved.</p>
+            </div>
+        </div>
+		-->
+    </div>
+</footer>
+
+<div class="bottom-footer">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                <p class="text-center text-dark">© 2021-24 smsmartbooking  | All Rights Reserved. Design by <a href="https://www.installdirect.asia/"><b>InstallDirect</b></a></p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
